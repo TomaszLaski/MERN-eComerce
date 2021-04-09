0f26133c8b483f8e9327ccd830a4d2cdabf8547f
TomaszLaski <tomasz.laski0001@gmail.com>
2021-04-09 22:47:49 +0200

    add Rating Component, small fixes in the code

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import Product from "../components/Product"
+import Product from "../components/Product";
 import products from "../products";
 
 const HomeScreen = () => {
@@ -9,7 +9,7 @@ const HomeScreen = () => {
 			<h1>Latest products</h1>
 			<Row>
 				{products.map((product) => (
-					<Col sm={12} md={6} lg={4}>
+					<Col key={product._id} sm={12} md={6} lg={4}>
 						<Product product={product} />
 					</Col>
 				))}

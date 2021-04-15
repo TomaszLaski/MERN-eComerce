45d96954532565199385d8d9ad057001219e7c77
TomaszLaski <tomasz.laski0001@gmail.com>
2021-04-15 20:43:57 +0200

    add user authentication endpoint

---
+++ backend/controlers/userController.js
@@ -0,0 +1,26 @@
+import AsyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
+
+// @desc auth user & get token
+// @route   POST /api/users/login
+// @access public
+const authUser = AsyncHandler(async (req, res) => {
+	const { email, password } = req.body;
+
+	const user = await User.findOne({ email });
+
+	if (user && (await user.matchPassword(password))) {
+		res.json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			isAdmin: user.isAdmin,
+			token: null,
+		});
+	} else {
+		res.status(401);
+		throw new Error("inwalid email or password");
+	}
+});
+
+export { authUser };

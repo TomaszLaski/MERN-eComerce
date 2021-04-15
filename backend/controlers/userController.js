cfe3b0016940ea4b8cc15bc6832a3bd37d3a9b07
TomaszLaski <tomasz.laski0001@gmail.com>
2021-04-15 21:50:07 +0200

    custom authentication middleware

--- backend/controlers/userController.js
+++ backend/controlers/userController.js
@@ -20,8 +20,27 @@ const authUser = AsyncHandler(async (req, res) => {
 		});
 	} else {
 		res.status(401);
-		throw new Error("inwalid email or password");
+		throw new Error("invalid email or password");
 	}
 });
 
-export { authUser };
+// @desc Get user profile
+// @route   GET /api/users/profile
+// @access Private
+const getUsersProfile = AsyncHandler(async (req, res) => {
+	const user = await User.findById(req.user._id);
+
+	if (user) {
+		res.json({
+			_id: user._id,
+			name: user.name,
+			email: user.email,
+			isAdmin: user.isAdmin,
+		});
+	} else {
+		res.status(404);
+		throw new Error("user not found");
+	}
+});
+
+export { authUser, getUsersProfile };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,9 @@
 import express from "express";
 const router = express.Router();
-import { authUser } from "../controlers/userController.js";
+import { authUser, getUsersProfile } from "../controlers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 router.post("/login", authUser);
+router.route("/profile").get(protect, getUsersProfile);
 
 export default router;
